5deb432dbe2960dd8e539e928365141d2828dd93
Christian Fröhlich <froehlich.ch@gmail.com>
2022-12-21 21:10:06 +0100

    add http header

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    logging.basicConfig(level=logging.INFO)
+    app.logger.info(request)
     return "hello world!"
     
 if __name__ == "__main__":
